86cca1e402ab667e9840a8dbd9e14061df75791b
BuildTools <93136063+300f20t@users.noreply.github.com>
2024-03-10 20:41:53 +0300

    fix

--- FS Launcher/obj/Debug/net7.0-windows/App.g.i.cs
+++ FS Launcher/obj/Debug/net7.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace FS_Launcher {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/FS Launcher;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FS Launcher;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
